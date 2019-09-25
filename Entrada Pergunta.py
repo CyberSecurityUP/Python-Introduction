b77f4a124343fe96e65db5be727aa37f17f7f950
Cr4zyC0d3 <34966120+CyberSecurityUP@users.noreply.github.com>
2019-09-25 13:10:38 -0300

    Update Entrada Pergunta.py

--- Entrada Pergunta.py
+++ Entrada Pergunta.py
@@ -1,6 +1,6 @@
-nome = str(input("Digite o seu Nome: "))
-idade = int(input("Digite a sua Idade: "))
-altura = float(input("Digite a sua altura: "))
-peso = float(input("Di"))
+nome = str(input("Digite o seu Nome: ")) #Armazenar variavel do tipo texto "Hello"
+idade = int(input("Digite a sua Idade: ")) #Do tipo número inteiro 10
+altura = float(input("Digite a sua altura: ")) #Do tipo número quebrado "1.80"
+peso = float(input("Digite o seu peso: ")) #Do tipo numero quebrado "1.80"
 
 print("O SEU NOME É", nome, "SUA IDADE É", idade, "A SUA ALTURA É", altura)
