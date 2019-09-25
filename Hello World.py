6f6243d843e743673a4961c20394cfbe2cab1dd1
Cr4zyC0d3 <34966120+CyberSecurityUP@users.noreply.github.com>
2019-09-25 12:58:17 -0300

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -1,5 +1,6 @@
+#Eu abro 2 váriavel sendo ela A e B, ao qual corresponde o valor "ola" e "tudo bem", esperando eles serem chamado pelo print
 a = "ola"
 b = "tudo bem"
-print(a, b)
+print(a, b) #Chamada das variavel
 print("OLA TUDO BEM COM VOCÊ")
 print("TUDO, E COM VOCÊ?")
