febff4341f07eee48b7d488452aa59934a44b01c
Cr4zyC0d3 <34966120+CyberSecurityUP@users.noreply.github.com>
2019-09-25 12:57:08 -0300

    Update Bibliotecas.py

--- Bibliotecas.py
+++ Bibliotecas.py
@@ -1,11 +1,15 @@
-#import time
+#Biblioteca time
+#Trabalhando com tempo, no caso ele utiliza o for para contar até 5, começando do 0 até o 4, em um time de 1 segundo
+#Basicamente ele conta 0 até 4 esperando 1 segundo
+import time
 
-#for i in range(5):
- #   print(i)
+for i in range(5):
+    print(i)
 
- #   time.sleep(1)
+    time.sleep(1)
 
-
+#Biblioteca tkinter
+#Fazendo um botão básico que ao clicar ele fecha a tela, nome do botão é OK
 from tkinter import *
 box = Button(text="Ok", command='exit')
 box.pack()
