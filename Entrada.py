5623bfb5ede507f99257c3522865e608eab1aaae
Cr4zyC0d3 <34966120+CyberSecurityUP@users.noreply.github.com>
2019-09-25 13:09:13 -0300

    Update Entrada.py

--- Entrada.py
+++ Entrada.py
@@ -1,9 +1,9 @@
 print("Ola a todos bem vindo ao curso")
-nome = input("Qual é o seu nome: ")
-print(nome)
+nome = input("Qual é o seu nome: ") #Abri a variavel nome ao qual vai receber uma entrada de dados, que seria no caso algum nome
+print(nome) #Aqui eu imprimo na tela o nome que foi armazenado na variavel "nome" pelo input
 
-num1 = input("Digite algum numero: ")
+num1 = input("Digite algum numero: ") #Abri a variavel num1 e num2 que está esperando algum valor 
 num2 = input("Digite algum numero: ")
 
-resultado = (num1 - num2)
-print("O resultado da soma é", resultado)
+resultado = (num1 - num2) #Aqui está a variavel resultado fazendo a subtração dos valores correspondente da variavel num1 - num2
+print("O resultado da soma é", resultado) #Aqui é o resultado da soma
